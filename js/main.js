f0c9f62e49118eaedc177dfad6e79aa576be6d89
Kayla Bowmaster <kay.bowmaster@gmail.com>
2019-04-29 21:25:15 -0400

    Replaced home page hero banner

---
+++ js/main.js
@@ -0,0 +1,43 @@
+<script>
+function modalClose() {
+  var x = document.getElementById("modal");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+</script>
+
+<script>
+function modalOpen() {
+  var x = document.getElementById("modal");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+</script>
+
+<script>
+function overlayHide() {
+  var x = document.getElementById("overlay");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+</script>
+
+<script>
+function overlayShow() {
+  var x = document.getElementById("overlay");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+</script>
