c5ba5fc7888f5baaf2f2dfe2dfe3ad9211d56f95
Alex Harvey <alex@alexharvey.tech>
2019-06-22 09:36:36 -0300

    added Background Image using graphql so gatsby likes it better

--- code/src/pages/index.js
+++ code/src/pages/index.js
@@ -8,12 +8,11 @@ import Footer from "../components/Footer";
 import PageHead from "../components/PageHead";
 
 import leaf from "../images/leaf.png";
-import studio2 from "../images/studio2.jpg";
 
 const Banner = styled.div`
   display: flex;
   flex-direction: column;
-  background-image: url(${studio2});
+  background-image: url(${props => props.bgSrc});
   min-height: 100%;
   width: 100%;
   background-repeat: no-repeat;
@@ -25,48 +24,63 @@ const Banner = styled.div`
   /* scale bg image proportionately */
   background-size: cover;
   /* ie8 workaround - http://louisremi.github.io/background-size-polyfill/ */
-  -ms-behavior: url(${studio2});
+  -ms-behavior: url(${props => props.bgSrc});
 `;
 
-const IndexPage = () => (
-  <>
-    <PageHead title="Home" />
-    <div className="main-container">
-      <Header />
+const IndexPage = ({ data }) => {
+  console.log(data);
+  return (
+    <>
+      <PageHead title="Home" />
+      <div className="main-container">
+        <Header />
 
-      <div className="banner-container">
-        <Banner>
-          <div className="banneroverlay">
-            <div className="text-center island-1-2 gutter">
-              <h1 className="pad-b">
-                historical and vintage creations
-                <br />
-                personalized patterns
-                <br />
-                handcrafted detail
-              </h1>
-              <Link className="micro button" to="/designs">
-                VIEW CUSTOM DESIGNS
-              </Link>
+        <div className="banner-container">
+          <Banner bgSrc={data.file.childImageSharp.fluid.src}>
+            <div className="banneroverlay">
+              <div className="text-center island-1-2 gutter">
+                <h1 className="pad-b">
+                  historical and vintage creations
+                  <br />
+                  personalized patterns
+                  <br />
+                  handcrafted detail
+                </h1>
+                <Link className="micro button" to="/designs">
+                  VIEW CUSTOM DESIGNS
+                </Link>
+              </div>
+              <div className="gutter tagline">
+                <img
+                  alt="leaf"
+                  style={{ verticalAlign: "middle", marginRight: ".325rem" }}
+                  className="leaf"
+                  src={leaf}
+                />
+                <span className="nano taglinetext">
+                  slow fashion, lovingly handcrafted in ontario, canada
+                </span>
+              </div>
             </div>
-            <div className="gutter tagline">
-              <img
-                alt="leaf"
-                style={{ verticalAlign: "middle", marginRight: ".325rem" }}
-                className="leaf"
-                src={leaf}
-              />
-              <span className="nano taglinetext">
-                slow fashion, lovingly handcrafted in ontario, canada
-              </span>
-            </div>
-          </div>
-        </Banner>
+          </Banner>
+        </div>
       </div>
-    </div>
 
-    <Footer />
-  </>
-);
+      <Footer />
+    </>
+  );
+};
+
+export const query = graphql`
+  query {
+    file(relativePath: { regex: "/studio2/" }) {
+      childImageSharp {
+        fluid {
+          src
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
