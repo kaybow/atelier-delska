b6a1798434474a5301c6ada43b7a23e16be5ed61
Kayla Bowmaster <kay.bowmaster@gmail.com>
2019-06-24 13:56:47 -0400

    Added import link line

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 import React from "react"
 
+import { Link, graphql, StaticQuery } from "gatsby";
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import form from "../images/form.svg";
